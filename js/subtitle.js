b539784e1187406726e17340912a6702343b70e3
Lucifer <52319328+hahazjk@users.noreply.github.com>
2021-08-03 21:52:34 +0900

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -52,10 +52,3 @@ function splitLetters(word) {
 
 changeWord();
 setInterval(changeWord, 3000);
-
-console.log(
-    `%c If you want know more about me %c Please mail to %c rockpanda666@gmail.com`,
-    'background:#F6ECD3 ; padding: 1px; border-radius: 3px 0 0 3px;  color: #000000',
-    'background:#DF5874 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
-    'background:transparent'
-)
